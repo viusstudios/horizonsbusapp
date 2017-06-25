68f4ebef221c6c8c995265b4552e52ea7386dd32
viusstudios <snatarajanx@gmail.com>
2017-06-26 10:20:31 +1200

    Set up Google Maps API (Hello World)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
-    // generic JQuery code
-    // as generic as the Paladins soundtrack
-});
+var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+}

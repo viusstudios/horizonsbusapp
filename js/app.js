0e064130eead2f7d7e2695527353fcc9ba99da13
viusstudios <snatarajanx@gmail.com>
2017-06-28 11:12:02 +1200

    Tested Google Maps further, now with markers

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-var map;
     function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
+        // LOCATIONS
+        var freybergHS = {lat: -40.337483, lng: 175.6254343};
+        var theSquare = {lat: -40.356301, lng: 175.611292};
+
+        // MAP
+        var palmyNth = new google.maps.Map(document.getElementById('map'), {
+            center: freybergHS,
+            zoom: 15
         });
-}
+
+        var freybergHSMarker = new google.maps.Marker({
+            position: freybergHS,
+            map: palmyNth
+        });
+
+        var theSquareMarker = new google.maps.Marker({
+            position: theSquare,
+            map: palmyNth
+        })
+
+    }
